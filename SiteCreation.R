5767ca7a8ad5b2f50f69d9bd33bae3550a898cac
ShKlinkenberg <s.klinkenberg@gmail.com>
2020-09-28 11:45:57 +0200

    creation script added

---
+++ SiteCreation.R
@@ -0,0 +1,12 @@
+new_site(
+  dir = "docs",
+  install_hugo = FALSE,
+  format = "toml",
+  sample = TRUE,
+  theme = "themefisher/dot-hugo-documentation-theme",
+  hostname = "github.com",
+  theme_example = TRUE,
+  empty_dirs = FALSE,
+  to_yaml = TRUE,
+  serve = interactive()
+)
